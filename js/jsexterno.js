77bbaf634e7130534b131b8c91f90005ecfef9e1
Mauricio <amcacuangoa@uce.edu.ec>
2024-11-15 14:36:34 +0000

    Taller_6

---
+++ js/jsexterno.js
@@ -0,0 +1,12 @@
+function cambiarTexto(idElemento, valor) {
+    document.getElementById(idElemento).innerText = valor;
+}
+function cambiarColor(idElemento, valor) {
+    document.getElementById(idElemento).style.color = valor
+}
+function cambiarTamano(idElemento, valor) {
+    document.getElementById(idElemento).style.fontSize = valor
+}
+function agregarElemento(idElemento, elementoHTML) {
+    document.getElementById(idElemento).innerHTML = document.getElementById(idElemento).innerHTML + elementoHTML
+}
